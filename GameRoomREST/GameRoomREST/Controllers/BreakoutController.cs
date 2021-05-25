0eaa5f49abdf0ecd4cc43a7adfa1e961c04ac185
perconey <perconey@gmail.com>
2021-05-25 09:31:30 +0200

    Adding REST Api

---
+++ GameRoomREST/GameRoomREST/Controllers/BreakoutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameRoomREST.Controllers
+{
+    public class BreakoutController : Controller
+    {
+        // GET: Breakout
+        public String Index()
+        {
+            StreamReader sr = System.IO.File.OpenText(Server.MapPath("Games/breakout.txt"));
+            String strContents = sr.ReadToEnd();
+
+            return strContents;
+        }
+    }
+}
---
+++ GameRoomREST/GameRoomREST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameRoomREST.WebApiApplication" Language="C#" %>
